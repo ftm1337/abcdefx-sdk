80f511ca41b7977a63081d20271710d251e9a65f
ftm1337 <ftm1337@protonmail.com>
2022-11-30 03:55:56 -0500

    Attempt to address #1

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -23,6 +23,16 @@ import { Token } from './token'
 
 let PAIR_ADDRESS_CACHE: { [token0Address: string]: { [token1Address: string]: string } } = {}
 
+
+
+
+
+
+
+
+
+
+
 export class Pair {
   public readonly liquidityToken: Token
   private readonly tokenAmounts: [TokenAmount, TokenAmount]
@@ -120,6 +130,67 @@ export class Pair {
     return token.equals(this.token0) ? this.reserve0 : this.reserve1
   }
 
+
+
+
+
+
+	///////////////////////////
+	///    @_SolidlyLib		///
+	///////////////////////////
+
+
+    function _SolidlyLib_k(x: number, y: number, _d0: number, _d1: number) {
+        let _x = x * 1e18 / _d0;
+        let _y = y * 1e18 / _d1;
+        let _a = (_x * _y) / 1e18;
+        let _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+        return _a * _b / 1e18;  // x3y+y3x >= k
+    }
+
+    function _SolidlyLib_f(x0: number, y: number) {
+        return x0*(y*y/1e18*y/1e18)/1e18+(x0*x0/1e18*x0/1e18)*y/1e18;
+    }
+
+    function _SolidlyLib_d( x0: number, y: number) {
+        return 3*x0*(y*y/1e18)/1e18+(x0*x0/1e18*x0/1e18);
+    }
+
+    function _SolidlyLib_get_y( x0: number,  xy: number,  y: number)  {
+        for ( let i = 0; i < 255; i++) {
+            let y_prev = y;
+            let k = _SolidlyLib_f(x0, y);
+            if (k < xy) {
+                 let dy = (xy - k)*1e18/_SolidlyLib_d(x0, y);
+                y = y + dy;
+            } else {
+                 let dy = (k - xy)*1e18/_SolidlyLib_d(x0, y);
+                y = y - dy;
+            }
+            if (y > y_prev) {
+                if (y - y_prev <= 1) {
+                    return y;
+                }
+            } else {
+                if (y_prev - y <= 1) {
+                    return y;
+                }
+            }
+        }
+        return y;
+    }
+
+	///////////////////////////
+	///    _SolidlyLib ^	///
+	///////////////////////////
+
+
+
+
+
+
+
+
   public getOutputAmount(inputAmount: TokenAmount): [TokenAmount, Pair] {
     invariant(this.involvesToken(inputAmount.token), 'TOKEN')
     if (JSBI.equal(this.reserve0.raw, ZERO) || JSBI.equal(this.reserve1.raw, ZERO)) {
@@ -127,12 +198,35 @@ export class Pair {
     }
     const inputReserve = this.reserveOf(inputAmount.token)
     const outputReserve = this.reserveOf(inputAmount.token.equals(this.token0) ? this.token1 : this.token0)
-    const inputAmountWithFee = JSBI.multiply(inputAmount.raw, _997)
-    const numerator = JSBI.multiply(inputAmountWithFee, outputReserve.raw)
-    const denominator = JSBI.add(JSBI.multiply(inputReserve.raw, _1000), inputAmountWithFee)
+    const inputAmountWithFee = JSBI.multiply(inputAmount.raw, _996)
+
+	let _SolidlyLib_tokenOut = inputAmount.token.equals(this.token0) ? this.token1 : this.token0;
+
+    const decimalsIn = JSBI.exponentiate(10, inputAmount.token.decimals);
+    const decimalsOut = JSBI.exponentiate(10, _SolidlyLib_tokenOut.decimals);
+
+
+    ///function _SolidlyLib_getAmountOut(amountIn: number, tokenIn: any, _reserve0: number, _reserve1: number) {
+    	///	Assumes amountIn to be deducted by fee
+        let xy =  _SolidlyLib_k(inputReserve, outputReserve, decimalsIn, decimalsOut);
+        let reserveInAdj = JSBI.divide( JSBI.multiply(inputReserve, 1e18), decimalsIn) ;
+        let reserveOutAdj = JSBI.divide( JSBI.multiply(outputReserve, 1e18), decimalsOut) ;
+        amountInAdj = JSBI.divide( JSBI.multiply(inputAmountWithFee, 1e18), decimalsIn) ;
+        let y = reserveOutAdj - _SolidlyLib_get_y(amountInAdj+reserveInAdj, xy, reserveOutAdj);
+        const _tokenOutAmount = JSBI.divide( JSBI.multiply(y, decimalsOut) , 1e18 );
+    ///}
+
+
+
+    ///const numerator = JSBI.multiply(inputAmountWithFee, outputReserve.raw)
+    ///const denominator = JSBI.add(JSBI.multiply(inputReserve.raw, _1000), inputAmountWithFee)
+
+
+
     const outputAmount = new TokenAmount(
       inputAmount.token.equals(this.token0) ? this.token1 : this.token0,
-      JSBI.divide(numerator, denominator)
+      _tokenOutAmount
+      ///JSBI.divide(numerator, denominator)
     )
     if (JSBI.equal(outputAmount.raw, ZERO)) {
       throw new InsufficientInputAmountError()
@@ -226,3 +320,235 @@ export class Pair {
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    **********************************
+    **********************************
+    **********************************
+    function getAmountOut(uint amountIn, address tokenIn) external view returns (uint) {
+        (uint _reserve0, uint _reserve1) = (reserve0, reserve1);
+        amountIn -= amountIn / fee; // remove fee from amount received
+        return _getAmountOut(amountIn, tokenIn, _reserve0, _reserve1);
+    }
+
+    function _getAmountOut(uint amountIn, address tokenIn, uint _reserve0, uint _reserve1) internal view returns (uint) {
+        uint xy =  _k(_reserve0, _reserve1);
+        _reserve0 = _reserve0 * 1e18 / decimals0;
+        _reserve1 = _reserve1 * 1e18 / decimals1;
+        (uint reserveA, uint reserveB) = tokenIn == token0 ? (_reserve0, _reserve1) : (_reserve1, _reserve0);
+        amountIn = tokenIn == token0 ? amountIn * 1e18 / decimals0 : amountIn * 1e18 / decimals1;
+        uint y = reserveB - _get_y(amountIn+reserveA, xy, reserveB);
+        return y * (tokenIn == token0 ? decimals1 : decimals0) / 1e18;
+    }
+
+    function _k(uint x, uint y) internal view returns (uint) {
+        uint _x = x * 1e18 / decimals0;
+        uint _y = y * 1e18 / decimals1;
+        uint _a = (_x * _y) / 1e18;
+        uint _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+        return _a * _b / 1e18;  // x3y+y3x >= k
+    }
+
+    function _f(uint x0, uint y) internal pure returns (uint) {
+        return x0*(y*y/1e18*y/1e18)/1e18+(x0*x0/1e18*x0/1e18)*y/1e18;
+    }
+
+    function _d(uint x0, uint y) internal pure returns (uint) {
+        return 3*x0*(y*y/1e18)/1e18+(x0*x0/1e18*x0/1e18);
+    }
+
+    function _get_y(uint x0, uint xy, uint y) internal pure returns (uint) {
+        for (uint i = 0; i < 255; i++) {
+            uint y_prev = y;
+            uint k = _f(x0, y);
+            if (k < xy) {
+                uint dy = (xy - k)*1e18/_d(x0, y);
+                y = y + dy;
+            } else {
+                uint dy = (k - xy)*1e18/_d(x0, y);
+                y = y - dy;
+            }
+            if (y > y_prev) {
+                if (y - y_prev <= 1) {
+                    return y;
+                }
+            } else {
+                if (y_prev - y <= 1) {
+                    return y;
+                }
+            }
+        }
+        return y;
+    }
+    **********************************
+    **********************************
+    **********************************
+
+const reserve0 = 100e18;
+const reserve1 = 90e18;
+const decimals0 = 1e18;
+const decimals1 = 1e18;
+const fee = 5;
+const token0 = null;
+const token1 = null;
+function _SolidlyLibgetAmountOut(amountIn, tokenIn) {
+    let _reserve0 = reserve0;
+    let _reserve1 = reserve1;
+    amountIn -= amountIn / fee; // remove fee from amount received
+    return _SolidlyLib_getAmountOut(amountIn, tokenIn, _reserve0, _reserve1);
+}
+function _SolidlyLib_getAmountOut(amountIn, tokenIn, _reserve0, _reserve1) {
+    let xy = _SolidlyLib_k(_reserve0, _reserve1);
+    _reserve0 = _reserve0 * 1e18 / decimals0;
+    _reserve1 = _reserve1 * 1e18 / decimals1;
+    let reserveA = tokenIn == token0 ? _reserve0 : _reserve1;
+    let reserveB = tokenIn == token0 ? _reserve1 : _reserve0;
+    amountIn = tokenIn == token0 ? amountIn * 1e18 / decimals0 : amountIn * 1e18 / decimals1;
+    let y = reserveB - _SolidlyLib_get_y(amountIn + reserveA, xy, reserveB);
+    return y * (tokenIn == token0 ? decimals1 : decimals0) / 1e18;
+}
+function _SolidlyLib_k(x, y) {
+    let _x = x * 1e18 / decimals0;
+    let _y = y * 1e18 / decimals1;
+    let _a = (_x * _y) / 1e18;
+    let _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+    return _a * _b / 1e18; // x3y+y3x >= k
+}
+function _SolidlyLib_f(x0, y) {
+    return x0 * (y * y / 1e18 * y / 1e18) / 1e18 + (x0 * x0 / 1e18 * x0 / 1e18) * y / 1e18;
+}
+function _SolidlyLib_d(x0, y) {
+    return 3 * x0 * (y * y / 1e18) / 1e18 + (x0 * x0 / 1e18 * x0 / 1e18);
+}
+function _SolidlyLib_get_y(x0, xy, y) {
+    for (let i = 0; i < 255; i++) {
+        let y_prev = y;
+        let k = _SolidlyLib_f(x0, y);
+        if (k < xy) {
+            let dy = (xy - k) * 1e18 / _SolidlyLib_d(x0, y);
+            y = y + dy;
+        }
+        else {
+            let dy = (k - xy) * 1e18 / _SolidlyLib_d(x0, y);
+            y = y - dy;
+        }
+        if (y > y_prev) {
+            if (y - y_prev <= 1) {
+                return y;
+            }
+        }
+        else {
+            if (y_prev - y <= 1) {
+                return y;
+            }
+        }
+    }
+    return y;
+}
+
+    **********************************
+    **********************************
+    **********************************
+
+const reserve0=100e18;
+const reserve1=90e18;
+const decimals0=1e18;
+const decimals1=1e18;
+const fee=5;
+const token0: any = null;
+const token1: any = null;
+
+    function _SolidlyLibgetAmountOut(amountIn: number,  tokenIn: any) {
+        let _reserve0 = reserve0;
+        let _reserve1 = reserve1;
+        amountIn -= amountIn / fee; // remove fee from amount received
+        return _SolidlyLib_getAmountOut(amountIn, tokenIn, _reserve0, _reserve1);
+    }
+
+    function _SolidlyLib_getAmountOut(amountIn: number, tokenIn: any, _reserve0: number, _reserve1: number) {
+        let xy =  _SolidlyLib_k(_reserve0, _reserve1);
+        _reserve0 = _reserve0 * 1e18 / decimals0;
+        _reserve1 = _reserve1 * 1e18 / decimals1;
+        let reserveA = tokenIn == token0 ? _reserve0 : _reserve1;
+        let reserveB = tokenIn == token0 ? _reserve1 : _reserve0;
+        amountIn = tokenIn == token0 ? amountIn * 1e18 / decimals0 : amountIn * 1e18 / decimals1;
+        let y = reserveB - _SolidlyLib_get_y(amountIn+reserveA, xy, reserveB);
+        return y * (tokenIn == token0 ? decimals1 : decimals0) / 1e18;
+    }
+
+    function _SolidlyLib_k(x: number, y: number) {
+        let _x = x * 1e18 / decimals0;
+        let _y = y * 1e18 / decimals1;
+        let _a = (_x * _y) / 1e18;
+        let _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+        return _a * _b / 1e18;  // x3y+y3x >= k
+    }
+
+    function _SolidlyLib_f(x0: number, y: number) {
+        return x0*(y*y/1e18*y/1e18)/1e18+(x0*x0/1e18*x0/1e18)*y/1e18;
+    }
+
+    function _SolidlyLib_d( x0: number, y: number) {
+        return 3*x0*(y*y/1e18)/1e18+(x0*x0/1e18*x0/1e18);
+    }
+
+    function _SolidlyLib_get_y( x0: number,  xy: number,  y: number)  {
+        for ( let i = 0; i < 255; i++) {
+            let y_prev = y;
+            let k = _SolidlyLib_f(x0, y);
+            if (k < xy) {
+                 let dy = (xy - k)*1e18/_SolidlyLib_d(x0, y);
+                y = y + dy;
+            } else {
+                 let dy = (k - xy)*1e18/_SolidlyLib_d(x0, y);
+                y = y - dy;
+            }
+            if (y > y_prev) {
+                if (y - y_prev <= 1) {
+                    return y;
+                }
+            } else {
+                if (y_prev - y <= 1) {
+                    return y;
+                }
+            }
+        }
+        return y;
+    }
+
+
+    **********************************
+    **********************************
+    **********************************
+    */
