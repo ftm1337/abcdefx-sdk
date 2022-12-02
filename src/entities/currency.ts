7482408ad49dcafd483a09ec68feff49d2a64b19
ftm1337 <ftm1337@protonmail.com>
2022-12-01 23:47:07 -0500

    Update to SDK v0.0.7

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, '', 'NativeGasCoin')	//Blank Symbol looks cute, UwU
+  public static readonly ETHER: Currency = new Currency(18, ' ', 'NativeGasCoin')	//Blank Symbol looks cute, UwU
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
